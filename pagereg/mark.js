6c00d7bd155fd91c9af7bc69c0d3a149e4cafcb4
lisnb <lisnb.h@hotmail.com>
2014-09-16 18:37:05 +0800

    fresh start

--- pagereg/mark.js
+++ pagereg/mark.js
@@ -2,7 +2,7 @@
  * @Author: LiSnB
  * @Date:   2014-08-14 16:46:34
  * @Last Modified by:   LiSnB
- * @Last Modified time: 2014-08-14 17:50:13
+ * @Last Modified time: 2014-09-15 20:57:13
  */
 
 
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	// xhr.open("GET", "localhost/pagereg/mark.php?url=baidu", true);
 	var url = chrome.extension.getBackgroundPage().url;
 	// alert(url);
-	xhr.open('GET',"http://localhost/pagereg/mark.php?url="+url,"true")
+	xhr.open('GET',"http://172.22.0.24/wde-ie-label/Tristor/pagereg/mark.php?url="+url,"true")
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState == 4) {
 			// alert(xhr.responseText);
